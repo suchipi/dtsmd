fbe1c1979235e7754dc9187b9890c714310839f1
Lily Skye <me@suchipi.com>
2024-01-21 21:38:30 -0700

    more groundwork

---
+++ src/cli.ts
@@ -0,0 +1,57 @@
+import * as fs from "fs";
+import * as clefairy from "clefairy";
+import { Path } from "nice-path";
+import * as dtsmd from "./index";
+
+clefairy.run(
+  {
+    i: clefairy.optionalString,
+    inputFile: clefairy.optionalString,
+    o: clefairy.optionalString,
+    outputFile: clefairy.optionalString,
+  },
+  async function main(opts, ...args) {
+    const inputFile = opts.i || opts.inputFile || null;
+    const outputFile = opts.o || opts.outputFile || null;
+
+    let source = "";
+    if (inputFile == null) {
+      if (process.stdin.isTTY) {
+        throw new Error(
+          "Please either pipe data to stdin or provide an input file"
+        );
+      } else {
+        source = fs.readFileSync(process.stdin.fd, "utf-8");
+      }
+    } else {
+      source = await fs.promises.readFile(inputFile, "utf-8");
+    }
+
+    let fileName: string;
+    if (typeof inputFile === "string") {
+      const inputPath = new Path(inputFile);
+      if (inputPath.isAbsolute()) {
+        fileName = inputFile;
+      } else {
+        const resolvedFromCwd = new Path(process.cwd(), inputPath).normalize();
+        if (resolvedFromCwd.isAbsolute()) {
+          fileName = inputFile;
+        } else {
+          // If they got here, it means readFile succeeded, but path
+          // normalization didn't? Just accept it I guess
+          fileName = inputFile;
+        }
+      }
+    } else {
+      fileName = "stdin";
+    }
+
+    const result = dtsmd.processSource(source, { fileName });
+
+    if (typeof outputFile === "string") {
+      await fs.promises.writeFile(outputFile, result);
+    } else {
+      fs.writeFileSync(process.stdout.fd, result);
+    }
+  }
+);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hi");
+import * as ee from "equivalent-exchange";
+import { formatAst } from "pretty-print-ast";
+
+export type Options = {
+  fileName?: string;
+};
+
+export function processSource(tsSource: string, options?: Options): string {
+  const ast = ee.parse(tsSource, {
+    typeSyntax: "typescript",
+    jsxEnabled: true,
+    fileName: options?.fileName,
+  });
+  console.log(formatAst(ast));
+
+  return "placeholder";
+}
