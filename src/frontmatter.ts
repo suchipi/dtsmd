d7c31262bdea734a6e91517269ccbb571fe87e18
Lily Skye <git@suchipi.tv>
2025-01-29 17:34:13 -0700

    some refactors

--- src/frontmatter.ts
+++ src/frontmatter.ts
@@ -1,14 +1,11 @@
 import * as ee from "equivalent-exchange";
 import * as YAML from "yaml";
-import { normalizeIndentation } from "./indent-utils";
+import { commentsToString } from "./comments-to-string";
 
 export type Frontmatter = {
   raw: string;
   parsed: { [key: PropertyKey]: any };
 };
-
-const debugLog = console.warn;
-// const debugLog: typeof console.warn = () => {};
 
 export function getFrontmatter(ast: ee.types.Program): null | Frontmatter {
   const statements = ast.body;
@@ -16,7 +13,6 @@ export function getFrontmatter(ast: ee.types.Program): null | Frontmatter {
   let firstComments: Array<ee.types.Comment> = [];
   if (statements.length === 0) {
     if (ast.innerComments && ast.innerComments.length > 0) {
-      debugLog("found first comment as body innerComments");
       firstComments.push(...ast.innerComments);
     }
   } else {
@@ -25,7 +21,6 @@ export function getFrontmatter(ast: ee.types.Program): null | Frontmatter {
       firstStatement.leadingComments &&
       firstStatement.leadingComments.length > 0
     ) {
-      debugLog("found first comment as first statement leading comments");
       firstComments.push(...firstStatement.leadingComments);
     }
   }
@@ -48,34 +43,4 @@ export function getFrontmatter(ast: ee.types.Program): null | Frontmatter {
   }
 
   return null;
-}
-
-function commentsToString(comments: Array<ee.types.Comment>) {
-  let text = "";
-  for (const comment of comments) {
-    switch (comment.type) {
-      case "CommentLine": {
-        text += comment.value + "\n";
-        break;
-      }
-      case "CommentBlock": {
-        if (/^\*\s/.test(comment.value)) {
-          // doc comment
-          text += comment.value
-            .replace(/^\*/, "")
-            .split("\n")
-            .map((line) => line.replace(/^(\s)\*/, "$1"))
-            .join("\n");
-        } else {
-          text += comment.value;
-        }
-        break;
-      }
-    }
-  }
-  if (/[\n\r]/.test(text)) {
-    text = normalizeIndentation(text, { output: "spaces", tabSize: 2 });
-  }
-  debugLog("commentsToString result", text);
-  return text;
 }
